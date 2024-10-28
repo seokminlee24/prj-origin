3b4e41ac7e43adfe8a7c2255a7e2fa73011312fa
seokminlee24 <lsm67286564@gmail.com>
2024-10-28 22:11:40 +0900

    게시물 추가

---
+++ src/main/java/com/example/prjorigin/controller/BoardController.java
@@ -0,0 +1,28 @@
+package com.example.prjorigin.controller;
+
+import com.example.prjorigin.dto.Board;
+import com.example.prjorigin.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("board")
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService service;
+
+    @GetMapping("new")
+    public void newBoard() {
+
+    }
+
+    @PostMapping("new")
+    public String newBoard(Board board) {
+        service.add(board);
+
+        return "redirect:/board/new";
+    }
+}
---
+++ src/main/java/com/example/prjorigin/mapper/BoardMapper.java
@@ -0,0 +1,15 @@
+package com.example.prjorigin.mapper;
+
+import com.example.prjorigin.dto.Board;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+    @Insert("""
+            INSERT INTO board
+            (title, content, writer)
+            VALUES (#{title}, #{content}, #{writer})
+            """)
+    int insert(Board board);
+}
