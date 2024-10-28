55450decfae1839da9918c876a6a0d33d8fa3be8
seokminlee24 <lsm67286564@gmail.com>
2024-10-28 20:36:23 +0900

    first commit

---
+++ src/test/java/com/example/prjorigin/PrjOriginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prjorigin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjOriginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
