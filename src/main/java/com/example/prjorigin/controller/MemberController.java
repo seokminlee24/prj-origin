6a62cbcb3d092e06dc9b8425b145c66e83a2b19b
seokminlee24 <lsm67286564@gmail.com>
2024-10-29 23:33:11 +0900

    암호 본인 계정 수정

--- src/main/java/com/example/prjorigin/controller/MemberController.java
+++ src/main/java/com/example/prjorigin/controller/MemberController.java
@@ -79,8 +79,17 @@ public class MemberController {
     }
 
     @GetMapping("edit")
-    public void edit(String id, Model model) {
-        model.addAttribute("member", service.info(id));
+    public String edit(String id, Model model,
+                       RedirectAttributes rttr,
+                       @SessionAttribute("loggedInMember") Member member) {
+        if (service.hasAccess(id, member)) {
+            model.addAttribute("member", service.info(id));
+            return null;
+        } else {
+            rttr.addFlashAttribute("message", Map.of("type", "danger",
+                    "text", "권한이 없습니다."));
+            return "redirect:/member/login";
+        }
     }
 
     @PostMapping("edit")
@@ -103,23 +112,39 @@ public class MemberController {
     }
 
     @GetMapping("edit-password")
-    public String editPassword(String id, Model model) {
-        model.addAttribute("id", id);
-        return "/member/editPassword";
+    public String editPassword(String id, Model model,
+                               RedirectAttributes rttr,
+                               @SessionAttribute("loggedInMember") Member member) {
+        if (service.hasAccess(id, member)) {
+            model.addAttribute("id", id);
+            return "/member/editPassword";
+        } else {
+            rttr.addFlashAttribute("message", Map.of("type", "danger",
+                    "text", "권한이 없습니다."));
+            return "redirect:/member/login";
+        }
     }
 
     @PostMapping("edit-password")
-    public String editPasswordProcess(String id, String oldPassword, String newPassword, RedirectAttributes rttr) {
-        if (service.updatePassword(id, oldPassword, newPassword)) {
-            rttr.addFlashAttribute("message", Map.of("type", "success",
-                    "text", "패스워드 변경 했습니다."));
-            rttr.addAttribute("id", id);
-            return "redirect:/member/view";
+    public String editPasswordProcess(String id, String oldPassword, String newPassword,
+                                      RedirectAttributes rttr,
+                                      @SessionAttribute("loggedInMember") Member member) {
+        if (service.hasAccess(id, member)) {
+            if (service.updatePassword(id, oldPassword, newPassword)) {
+                rttr.addFlashAttribute("message", Map.of("type", "success",
+                        "text", "패스워드 변경 했습니다."));
+                rttr.addAttribute("id", id);
+                return "redirect:/member/view";
+            } else {
+                rttr.addFlashAttribute("message", Map.of("type", "warning",
+                        "text", "패스워드 변경되지 않습니다."));
+                rttr.addAttribute("id", id);
+                return "redirect:/member/edit-password";
+            }
         } else {
-            rttr.addFlashAttribute("message", Map.of("type", "warning",
-                    "text", "패스워드 변경되지 않습니다."));
-            rttr.addAttribute("id", id);
-            return "redirect:/member/edit-password";
+            rttr.addFlashAttribute("message", Map.of("type", "danger",
+                    "text", "권한이 없습니다."));
+            return "redirect:/member/login";
         }
     }
 
